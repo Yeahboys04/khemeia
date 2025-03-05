ad32dfd246a8cc6ffeac64392bf62ec88d11bd41
Ryan <ryankorban2004@gmail.com>
2025-03-05 20:31:22 +0100

    Refonte graphique de la recherche

--- src/Controller/TracabilityController.php
+++ src/Controller/TracabilityController.php
@@ -19,26 +19,26 @@ class TracabilityController extends AbstractController
     #[Route('/tracability', name: 'tracability')]
     public function index(EntityManagerInterface $entityManager, TokenStorageInterface $tokenStorage): Response
     {
-        //try {
-        //Initialise le repository pour la base de données
-        $repositoryTracability = $entityManager->getRepository(Tracability::class);
+        try {
+            // Initialise le repository pour la base de données
+            $repositoryTracability = $entityManager->getRepository(Tracability::class);
 
-        $user = $tokenStorage->getToken()->getUser();
+            $user = $tokenStorage->getToken()->getUser();
 
-        $tracability = $repositoryTracability->findTracabilityByUser($user->getIdUser());
+            $tracability = $repositoryTracability->findTracabilityByUser($user->getIdUser());
 
-        return $this->render('tracability/index.html.twig', [
-            'tracability' => $tracability,
-        ]);
-        //}
-        // S'il y a tout autre exception
-        // catch (\Exception $e) {
-        //     $this->addFlash('error',
-        //         'Attention, une erreur est survenue.'
-        //         .' Contactez votre administrateur.');
-        //     //on redirige vers la page d'accueil
-        //     return $this->redirectToRoute('home_page');
-        // }
+            return $this->render('tracability/index.html.twig', [
+                'tracability' => $tracability,
+            ]);
+        }
+            // S'il y a toute autre exception
+        catch (\Exception $e) {
+            $this->addFlash('error',
+                'Attention, une erreur est survenue.'
+                .' Contactez votre administrateur.');
+            //on redirige vers la page d'accueil
+            return $this->redirectToRoute('home_page');
+        }
     }
 
     #[Route('/tracability/cart/{id}', name: 'tracability_add')]
@@ -48,32 +48,32 @@ class TracabilityController extends AbstractController
         EntityManagerInterface $entityManager,
         TokenStorageInterface $tokenStorage
     ): Response {
-        //try {
-        $user = $tokenStorage->getToken()->getUser();
+        try {
+            $user = $tokenStorage->getToken()->getUser();
 
-        $repositoryStoragecard = $entityManager->getRepository(Storagecard::class);
-        $storagecard = $repositoryStoragecard->find($id);
+            $repositoryStoragecard = $entityManager->getRepository(Storagecard::class);
+            $storagecard = $repositoryStoragecard->find($id);
 
-        $tracability = new Tracability();
-        $tracability->setRetiredate(new DateTime());
-        $tracability->setRetirequantity(null);
-        $tracability->setIdStoragecard($storagecard);
-        $tracability->setIdUser($user);
-        $entityManager->persist($tracability);
-        $entityManager->flush();
+            $tracability = new Tracability();
+            $tracability->setRetiredate(new DateTime());
+            $tracability->setRetirequantity(null);
+            $tracability->setIdStoragecard($storagecard);
+            $tracability->setIdUser($user);
+            $entityManager->persist($tracability);
+            $entityManager->flush();
 
-        $this->addFlash('success',
-            'Le produit a bien été ajouté à votre historique.');
-        return $this->redirectToRoute('tracability_search');
-        //}
-        // S'il y a tout autre exception
-        // catch (\Exception $e) {
-        //     $this->addFlash('error',
-        //         'Attention, une erreur est survenue.'
-        //         .' Contactez votre administrateur.');
-        //     //on redirige vers la page d'accueil
-        //     return $this->redirectToRoute('home_page');
-        // }
+            $this->addFlash('success',
+                'Le produit a bien été ajouté à votre historique.');
+            return $this->redirectToRoute('tracability_search');
+        }
+            // S'il y a toute autre exception
+        catch (\Exception $e) {
+            $this->addFlash('error',
+                'Attention, une erreur est survenue.'
+                .' Contactez votre administrateur.');
+            //on redirige vers la page d'accueil
+            return $this->redirectToRoute('home_page');
+        }
     }
 
     #[Route('/tracability/search', name: 'tracability_search')]
@@ -82,40 +82,74 @@ class TracabilityController extends AbstractController
         EntityManagerInterface $entityManager,
         TokenStorageInterface $tokenStorage
     ): Response {
-        //try {
-        //Initialise le repository pour la base de données
-        $user = $tokenStorage->getToken()->getUser();
+        try {
+            // Initialise le repository pour la base de données
+            $user = $tokenStorage->getToken()->getUser();
 
-        $repositoryStoragecard = $entityManager->getRepository(Storagecard::class);
-        $storagecard = new Storagecard();
+            $repositoryStoragecard = $entityManager->getRepository(Storagecard::class);
+            $storagecard = new Storagecard();
 
-        $formStoragecard = $this->createForm(SearchType::class, $storagecard, [
-            'method' => 'POST',
-        ]);
-        $formStoragecard->handleRequest($request);
-        if ($formStoragecard->isSubmitted() && $formStoragecard->isValid()) {
-            $storagecard = $formStoragecard->get('idChimicalproduct')->getData();
-            $site = $formStoragecard->get('idSite')->getData();
-
-
-            //Cherche tous les produits qui correspondent à la recherche
-            $storagecards = $repositoryStoragecard->loadStorageCardBySite($site, $storagecard);
-
-            //on retourne la page avec les informations trouvées
-            return $this->render('tracability/cart.html.twig', [
-                'formStoragecard' => $formStoragecard->createView(),
-                'storagecards' => $storagecards
+            $formStoragecard = $this->createForm(SearchType::class, $storagecard, [
+                'method' => 'POST',
             ]);
+
+            $formStoragecard->handleRequest($request);
+
+            if ($formStoragecard->isSubmitted() && $formStoragecard->isValid()) {
+                // Récupère le type de recherche (produit ou CAS)
+                $searchType = $formStoragecard->get('searchType')->getData();
+
+                // Récupère l'option de recherche sur tous les sites
+                $searchAll = $formStoragecard->get('searchAll')->getData();
+
+                // Récupère le site sélectionné seulement si on ne cherche pas sur tous les sites
+                $site = null;
+                if ($searchAll === 'non') {
+                    $site = $formStoragecard->get('idSite')->getData();
+                }
+
+                // Récupère le produit chimique selon le type de recherche
+                if ($searchType === 'cas') {
+                    // Recherche par numéro CAS
+                    $chimicalProduct = $formStoragecard->get('casSearch')->getData();
+                } else {
+                    // Recherche par nom de produit (par défaut)
+                    $chimicalProduct = $formStoragecard->get('idChimicalproduct')->getData();
+                }
+
+                // Si aucun produit n'est sélectionné, affiche un message d'erreur
+                if (!$chimicalProduct) {
+                    $this->addFlash('error', 'Veuillez sélectionner un produit.');
+                    return $this->render('tracability/cart.html.twig', [
+                        'formStoragecard' => $formStoragecard->createView(),
+                    ]);
+                }
+
+                // Recherche les produits correspondants (selon option tous sites ou site spécifique)
+                if ($searchAll === 'oui') {
+                    // Fonction à créer dans le repository ou utilisation de findBy
+                    $storagecards = $repositoryStoragecard->loadStorageCardsByCAS($chimicalProduct->getCasnumber());
+                } else {
+                    $storagecards = $repositoryStoragecard->loadStorageCardBySite($site, $chimicalProduct);
+                }
+
+                // Rend la page avec les résultats
+                return $this->render('tracability/cart.html.twig', [
+                    'formStoragecard' => $formStoragecard->createView(),
+                    'storagecards' => $storagecards,
+                    'site' => $site
+                ]);
+            }
         }
-        //}
-        // S'il y a tout autre exception
-        // catch (\Exception $e) {
-        //     $this->addFlash('error',
-        //         'Attention, une erreur est survenue.'
-        //         .' Contactez votre administrateur.');
-        //     //on redirige vers la page d'accueil
-        //     return $this->redirectToRoute('home_page');
-        // }
+            // S'il y a toute autre exception
+        catch (\Exception $e) {
+            $this->addFlash('error',
+                'Attention, une erreur est survenue.'
+                .' Contactez votre administrateur.');
+            //on redirige vers la page d'accueil
+            return $this->redirectToRoute('home_page');
+        }
+
         return $this->render('tracability/cart.html.twig', [
             'formStoragecard' => $formStoragecard->createView(),
         ]);
